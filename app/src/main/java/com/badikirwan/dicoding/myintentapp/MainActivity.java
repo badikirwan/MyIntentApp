19471679403865a8ab3a97c225dd127f797f30a4
badikirwan <badikirwan@gmail.com>
2018-07-16 19:35:49 +0700

    Initial commit

---
+++ app/src/main/java/com/badikirwan/dicoding/myintentapp/MainActivity.java
@@ -0,0 +1,39 @@
+package com.badikirwan.dicoding.myintentapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnMoveActivity, btnMoveWithDataActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnMoveActivity = (Button) findViewById(R.id.btn_move_activity);
+        btnMoveActivity.setOnClickListener(this);
+        btnMoveWithDataActivity = (Button) findViewById(R.id.btn_move_activity_data);
+        btnMoveWithDataActivity.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_move_activity:
+                Intent moveIntent = new Intent(MainActivity.this, MoveActivity.class);
+                startActivity(moveIntent);
+                break;
+            case R.id.btn_move_activity_data:
+                Intent moveWithDataIntent = new Intent(MainActivity.this, MoveWithDataActivity.class);
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Badik Irwan");
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 21);
+                startActivity(moveWithDataIntent);
+                break;
+        }
+
+    }
+}
